1811b0e20bb54a57a58375495c597732fca114cc
kyrylo-piddubnyi <Kyrylo.Piddubnyi@epicgames.com>
2020-02-01 19:39:33 +0200

    Connect to Postgres DB

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,37 @@
 import express from "express";
 
-import {
-  addUser,
-  deleteUser,
-  getAutoSuggestUsers,
-  getUser,
-  updateUser
-} from "./controllers/user";
+import { UserController } from "./controllers/user";
+import { DataBase } from "./database";
 import { schema, validate } from "./validation";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.set("db-host", process.env.DB_HOST || "localhost");
+app.set("db-port", process.env.DB_PORT || "5432");
+app.set("db-user", process.env.DB_USER || "kyrylopiddubnyi");
+app.set("db-password", process.env.DB_PASSWORD || "");
+app.set("db-users-table-name", process.env.DB_USER_TABLE_NAME || "users");
 app.use(express.json());
 
-app.post("/users", validate(schema), addUser);
-app.patch("/users/:id", validate(schema), updateUser);
-app.get("/users", getAutoSuggestUsers);
-app.get("/users/:id", getUser);
-app.delete("/users/:id", deleteUser);
+const db = new DataBase();
+
+db.init({
+  config: {
+    host: app.get("db-host"),
+    port: app.get("db-port"),
+    username: app.get("db-user"),
+    dialect: "postgres"
+  },
+  tableName: app.get("db-users-table-name")
+});
+
+const userController = new UserController(db);
+
+app.post("/users", validate(schema), userController.addUser);
+app.patch("/users/:id", validate(schema), userController.updateUser);
+app.get("/users", userController.getAutoSuggestUsers);
+app.get("/users/:id", userController.getUser);
+app.delete("/users/:id", userController.deleteUser);
 
 export default app;
